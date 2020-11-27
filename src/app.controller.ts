46d8279488ee474792903cc2fe5279ea6884cf52
Miguel Savignano <migue.masx@gmail.com>
2020-11-27 12:43:40 +0100

    koa adapter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,9 +3,9 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService) {}
 
-  @Get()
+  @Get('/users')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { KoaAdapter, NestKoaApplication } from 'nest-koa-adapter';
+import * as Koa from 'koa';
 import { AppModule } from './app.module';
 
+async function koaMiddleware(ctx, next) {
+  console.log(ctx.request.originalUrl);
+  await next();
+}
+
+const koa = new Koa();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestKoaApplication>(
+    AppModule,
+    new KoaAdapter(koa),
+  );
+  app.use(koaMiddleware);
   await app.listen(3000);
 }
 bootstrap();
